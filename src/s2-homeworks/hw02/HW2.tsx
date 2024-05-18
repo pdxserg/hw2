b2749f6c02f138c371d219a8734add738859da17
SergeyStarchenko <pdxserg@gmail.com>
2024-05-17 18:47:55 -0700

    delet test work!

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -40,9 +40,9 @@ export const filterAffairs = (affairs: AffairType[], filter: FilterType): any =>
 
     return affairs.map(el => el.priority === filter)// need to fix
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
+export const deleteAffair = (affairs: AffairType[], _id: number): any => { // need to fix any
 
-    return affairs // need to fix
+    return affairs.filter(el=> el._id !== _id) // need to fix
 }
 
   function HW2() {
