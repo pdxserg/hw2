cacf077f2d1f1ab56e7fed866d83d136d5bc675d
SergeyStarchenko <pdxserg@gmail.com>
2024-06-11 22:42:38 -0700

    hw4 finished

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        // className={open? s.nav: ""} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,6 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
+                        // className={open? s.nav: ""}
                         // className={...} // делает студент
                     >
                         Junior
@@ -47,6 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
+                        // className={open? s.nav: ""}
                         // className={...} // делает студент
                     >
                         Junior Plus
