1363d9939ff4aae5d30a71ab38e9fd5e9912b298
SergeyStarchenko <pdxserg@gmail.com>
2024-06-25 20:23:51 -0700

    hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -13,9 +13,6 @@ import s from './HW6.module.css'
 
 const HW6 = () => {
     const [value, setValue] = useState<string>('')
-const onblurHandler= (newValue:any)=>{
-
-}
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
