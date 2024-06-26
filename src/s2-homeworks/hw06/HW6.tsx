7b3f83a4dc60488a4d800ce1a015719246c62121
SergeyStarchenko <pdxserg@gmail.com>
2024-06-25 21:19:53 -0700

    hw6 work with local storage

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import { restoreState, saveState } from './localStorage/localStorage'
+import {restoreState, saveState} from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
@@ -12,55 +12,70 @@ import s from './HW6.module.css'
  */
 
 const HW6 = () => {
-    const [value, setValue] = useState<string   >('')
+	const [value, setValue] = useState<string>('')
 
-debugger
-    const save = () => {
-        saveState<string>('hw6-editable-span-value', value)
-        debugger
-    }
-    const restore = () => {
 
-         const  newValue= localStorage.getItem(value)|| "www"
-debugger
-        setValue(newValue)
-        // делают студенты
+	const save = () => {
+		saveState<string>('hw6-editable-span-value', value)
 
-    }
+	}
+	const restore = () => {
 
-    return (
-        <div id={'hw6'}>
-            <div className={s2.hwTitle}>Homework #6</div>
+			// let newValue = localStorage.getItem('hw6-editable-span-value')
+			// if (newValue) {
+			// 	setValue(JSON.parse(newValue))
+			// }
 
-            {/*демонстрация возможностей компоненты:*/}
-            <div className={s2.hw}>
-                <div className={s.editableSpanContainer}>
-                    <SuperEditableSpan
-                        id={'hw6-spanable-input'}
-                        value={value}
-                        onChangeText={setValue}
-                        spanProps={{
-                            id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
-                        }}
-                    />
-                </div>
 
-                <div className={s.buttonsContainer}>
-                    <SuperButton id={'hw6-save'} onClick={save}>
-                        Save to ls
-                    </SuperButton>
-                    <SuperButton
-                        id={'hw6-restore'}
-                        onClick={restore}
-                        xType={'secondary'}
-                    >
-                        Get from ls
-                    </SuperButton>
-                </div>
-            </div>
-        </div>
-    )
+		// useEffect(() => {
+		// 	let valueAsStringMax = localStorage.getItem('max')
+		// 	if (valueAsStringMax) {
+		// 		setNumMax(JSON.parse(valueAsStringMax))
+		// 	}
+		//
+		// }, []);
+		//
+		//
+		const newValue = localStorage.getItem('hw6-editable-span-value') || ""
+
+		setValue(newValue)
+		// делают студенты
+
+	}
+
+	return (
+		<div id={'hw6'}>
+			<div className={s2.hwTitle}>Homework #6</div>
+
+			{/*демонстрация возможностей компоненты:*/}
+			<div className={s2.hw}>
+				<div className={s.editableSpanContainer}>
+					<SuperEditableSpan
+						id={'hw6-spanable-input'}
+						value={value}
+						onChangeText={setValue}
+						spanProps={{
+							id: 'hw6-editable-span',
+							defaultText: 'enter text...',
+						}}
+					/>
+				</div>
+
+				<div className={s.buttonsContainer}>
+					<SuperButton id={'hw6-save'} onClick={save}>
+						Save to ls
+					</SuperButton>
+					<SuperButton
+						id={'hw6-restore'}
+						onClick={restore}
+						xType={'secondary'}
+					>
+						Get from ls
+					</SuperButton>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HW6
