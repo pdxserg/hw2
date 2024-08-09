dbea782789bc80336402d6a070ad597ec451a471
SergeyStarchenko <pdxserg@gmail.com>
2024-08-09 13:36:48 -0700

    HW8 test('sort name up') passed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,11 +4,11 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+                const copyState= state
+            return copyState.sort((a, b) => a.name.localeCompare(b.name)) // need to fix
         }
         case 'check': {
 
