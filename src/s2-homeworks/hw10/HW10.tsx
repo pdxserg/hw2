faf10a0ca7724f750d4f9bb2ad5af995409136ea
SergeyStarchenko <pdxserg@gmail.com>
2024-09-15 10:39:41 -0700

    HW10create useSelector

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import {useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
 /*
-* 1 - в файле loadingReducer.ts дописать типы и логику
+* 1 - в файле loadingReducer.ts дописать типы и логику ✅
 * 2 - получить isLoading из редакса
 * 3 - дописать функцию setLoading
 * 4 - сделать стили в соответствии с дизайном
@@ -15,7 +14,9 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+
+    // const isLoading = false
+    const isLoading = useSelector<AppStoreType>(state => state.loading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
