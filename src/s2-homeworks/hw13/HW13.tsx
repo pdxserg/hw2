6ab30d927d3395f10f14badc6caa8d6abf5ad635
SergeyStarchenko <pdxserg@gmail.com>
2024-11-30 08:57:56 -0800

    HW13  fix undeffined and null

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -9,7 +9,7 @@ import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
 
 /*
-* 1 - дописать функцию send
+* 1 - дописать функцию send ✅
 * 2 - дизэйблить кнопки пока идёт запрос
 * 3 - сделать стили в соответствии с дизайном
 * */
